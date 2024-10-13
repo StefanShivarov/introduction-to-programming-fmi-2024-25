3fd0480148a6c7052d8e595eb26c8719d925d913
Кристиян Мирославов Богданов <71849513+KristiyanBogdanov@users.noreply.github.com>
2024-10-13 20:09:56 +0300

    Add Task2

---
+++ Week 01/Pract. 01/Task2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   int a = 0, b = 0, c = 0, d = 0, sum = 0;
+
+   cout << "Numbers: ";
+   cin >> a >> b >> c >> d;
+
+   if (a % 2 == 0) { sum += a; }
+   if (b % 2 == 0) { sum += b; }
+   if (c % 2 == 0) { sum += c; }
+   if (d % 2 == 0) { sum += d; }
+
+   cout << "The sum is: " << sum << endl;
+
+   return 0;
+}
