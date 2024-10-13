0bf0fe7d9f6b94505476723cdc35753a00a1b0fd
Кристиян Мирославов Богданов <71849513+KristiyanBogdanov@users.noreply.github.com>
2024-10-13 20:37:59 +0300

    Add Task6

---
+++ Week 01/Pract. 01/Task6.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    unsigned int n;
+
+    cout << "N: "; cin >> n;
+
+    switch (n) {
+    case 1:
+        cout << "Monday" << endl;
+        break;
+    case 2:
+        cout << "Tuesday" << endl;
+        break;
+    case 3:
+        cout << "Wednesday" << endl;
+        break;
+    case 4:
+        cout << "Thursday" << endl;
+        break;
+    case 5:
+        cout << "Friday" << endl;
+        break;
+    case 6:
+        cout << "Saturday" << endl;
+        break;
+    case 7:
+        cout << "Sunday" << endl;
+        break;
+    default:
+        cout << "The n should be between 1 and 7" << endl;
+    }
+
+    return 0;
+}
