fa90de724875ce7c14b9cf9c5e9b8d0452edde5a
Zakhar Sobarshov <zakhar.zak@gmail.com>
2024-10-14 18:45:46 +0300

    Task 01

---
+++ Week 02/Pract. 02/Task01.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int prompt_number = 0;
+	int second_number = 0;
+	int sum = 0;
+
+	cout << "Please enter the number which will start a cycle: ";
+	cin >> prompt_number;
+	while (prompt_number) {
+		cin >> second_number;
+		sum += second_number;
+
+		--prompt_number;
+	}
+
+	cout << "Out: " << sum;
+}
