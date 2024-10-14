79d6bd9710c294ef2fd4c9dd64102f69cfb1edd4
Zakhar Sobarshov <zakhar.zak@gmail.com>
2024-10-14 20:45:31 +0300

    Task05

---
+++ Week 02/Pract. 02/Task05.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int prompt_number = 0;
+	int negative_number = 0;
+	int counter = 0;
+
+	cout << "Please enter the number which will start a cycle: ";
+	while (counter < 3) {
+		cin >> prompt_number;
+		
+		if (prompt_number >= 0) {
+			negative_number = 0;
+			counter = 0;
+		}
+
+		if (prompt_number < 0) {
+			if (negative_number > prompt_number) {
+				negative_number = prompt_number;
+			}
+
+			++counter;
+		}
+	}
+
+	cout << "Out: " << negative_number;
+}
