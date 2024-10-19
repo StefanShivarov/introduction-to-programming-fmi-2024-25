14038c1fd896d249d8ffc86f4613546303d3ac5d
Yasen Angelov <yse33@abv.bg>
2024-10-19 22:21:20 +0300

    Add Task02 solution

---
+++ Week 02/Pract. 02/Task02.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    unsigned int count = 0;
+    cin >> count;
+
+    int largest = 0;
+    for (int i = 0; i < count; i++) {
+        int current = 0;
+        cin >> current;
+
+        if (largest < current) largest = current;
+    }
+
+    cout << largest;
+}
