201d35fed1d4a49a26dcebfc5716fbea359adf6a
Yasen Angelov <yse33@abv.bg>
2024-10-19 22:22:48 +0300

    Add Task03 solution

---
+++ Week 02/Pract. 02/Task03.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num = 0;
+    cin >> num;
+
+    int factorial = 1;
+    while (num) {
+        factorial *= num--;
+    }
+
+    cout << factorial;
+}
