872dc00f2c2c33ac5b32da77837df3dfae6f60c4
Yasen Angelov <yse33@abv.bg>
2024-10-19 22:28:51 +0300

    Add Task04 solution

---
+++ Week 02/Pract. 02/Task04.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int sum = 0;
+    int positive_count = 0;
+
+    while (true) {
+        if (positive_count == 5) break;
+
+        int num = 0;
+        cin >> num;
+
+        if (num > 0) {
+            positive_count++;
+            sum += num;
+        }
+    }
+
+    cout << sum;
+}
