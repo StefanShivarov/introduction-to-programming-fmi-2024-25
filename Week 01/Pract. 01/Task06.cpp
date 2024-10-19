34a687695bcc84b488f2c9bcbf33108839b9452e
Niya Neykova <natealizardd@gmail.com>
2024-10-19 18:44:59 +0300

    Update Task06.cpp

--- Week 01/Pract. 01/Task06.cpp
+++ Week 01/Pract. 01/Task06.cpp
@@ -1,55 +1,37 @@
+//
+// Created by Niya Neykova on 11.10.24.
+//
+
 #include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    switch (n) {
+        case 1:
+            cout<<"Monday"<<endl;
+            break;
+        case 2:
+            cout<<"Tuesday"<<endl;
+            break;
+        case 3:
+            cout<<"Wednesday"<<endl;
+            break;
+        case 4:
+            cout<<"Thursday"<<endl;
+            break;
+        case 5:
+            cout<<"Friday"<<endl;
+            break;
+        case 6:
+            cout<<"Saturday"<<endl;
+            break;
+        case 7:
+            cout<<"Sunday"<<endl;
+            break;
+        default:
+            cout<<"Error. Number must be between 1 and 7"<<endl;
+    }
 
-int main()
-{
-	int day_number = 0;
-
-	std::cout << "Please enter number from 1 to 7: "; std::cin >> day_number;
-
-	switch (day_number) {
-	case 1:
-		{
-			std::cout << "Monday" << std::endl;
-
-			break;
-		}
-	case 2:
-		{
-			std::cout << "Tuesday" << std::endl;
-
-			break;
-		}
-	case 3:
-		{
-			std::cout << "Wednesday" << std::endl;
-
-			break;
-		}
-	case 4:
-		{
-			std::cout << "Thursday" << std::endl;
-
-			break;
-		}
-	case 5:
-		{
-			std::cout << "Friday" << std::endl;
-
-			break;
-		}
-	case 6:
-		{
-			std::cout << "Saturday" << std::endl;
-
-			break;
-		}
-	case 7:
-		{
-			std::cout << "Sunday" << std::endl;
-
-			break;
-		}
-}
-
-	return 0;
+    return 0;
 }
