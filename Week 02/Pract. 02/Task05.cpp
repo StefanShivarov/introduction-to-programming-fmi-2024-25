04331a2098e452af70ea35b89a3463260d911acc
Yasen Angelov <yse33@abv.bg>
2024-10-19 22:30:25 +0300

    Add Task05 solution

---
+++ Week 02/Pract. 02/Task05.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int smallest = 0;
+    int negative_in_a_row_count = 0;
+
+    while (true) {
+        if (negative_in_a_row_count == 3) break;
+
+        int num = 0;
+        cin >> num;
+
+        if (num < 0) {
+            negative_in_a_row_count++;
+            if (smallest > num) smallest = num;
+        } else {
+            negative_in_a_row_count = smallest = 0;
+        }
+    }
+
+    cout << smallest;
+}
