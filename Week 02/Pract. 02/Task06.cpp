415fe6e3c50099959896ae4042fa9986da6eea70
Yasen Angelov <yse33@abv.bg>
2024-10-19 22:34:11 +0300

    Add Task06 solution

---
+++ Week 02/Pract. 02/Task06.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n = 0, k = 0;
+    cin >> n >> k;
+
+    int sum = 0;
+    for (int i = 1; i <= n; i++) {
+        int num = k;
+        for (int j = 1; j < i; j++) {
+            num += k * static_cast<int>(pow(10, j));
+        }
+        sum += num;
+    }
+
+    cout << sum;
+}
