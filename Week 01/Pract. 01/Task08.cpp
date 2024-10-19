08c5eae1b21ae56ab86ba47e51f2ecc9ce3a9523
Niya Neykova <natealizardd@gmail.com>
2024-10-19 18:45:04 +0300

    Update Task08.cpp

--- Week 01/Pract. 01/Task08.cpp
+++ Week 01/Pract. 01/Task08.cpp
@@ -1,30 +1,34 @@
+//
+// Created by Niya Neykova on 12.10.24.
+//
+
 #include <iostream>
+using namespace std;
+int main() {
+    double a,b;
+    char c;
+    cin>>a>>c>>b;
 
-int main()
-{
-	float first_opearand = 0;
-	unsigned char prompt_operator;
-	float second_opearand = 0;
+    switch(c) {
+        case '+':
+            cout<<a+b<<endl;
+            break;
+        case '-':
+            cout<<a-b<<endl;
+            break;
+        case '*':
+            cout<<a*b<<endl;
+            break;
+        case '/':
+            if(b!=0) {
+                cout<<a/b<<endl;
+            }else {
+                cout<<"Error! You can't divide by 0!"<<endl;
+            }
+            break;
+        default:
+            cout<<"Error"<<endl;
+    }
 
-	std::cout << "Please enter first operand, operator and second operand: "; std::cin >> first_opearand >> prompt_operator >> second_opearand;
-
-	if (prompt_operator == '+') {
-		std::cout << first_opearand + second_opearand;
-	}
-
-	if (prompt_operator == '-') {
-		std::cout << first_opearand - second_opearand;
-	}
-
-	if (prompt_operator == '*') {
-		std::cout << first_opearand * second_opearand;
-	}
-
-	if (prompt_operator == '/' && second_opearand) {
-		std::cout << first_opearand / second_opearand;
-	} else if (prompt_operator == '/' && !second_opearand) {
-		std::cout << "Error! You can't divide by 0!" << std::endl;
-	}
-
-	return 0;
+    return 0;
 }
