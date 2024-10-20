e687f87a54f71b6b0d29359cf7e796295f5e770c
Кристиян Мирославов Богданов <71849513+KristiyanBogdanov@users.noreply.github.com>
2024-10-20 19:23:38 +0300

    Add Task7

---
+++ Week 02/Pract. 02/Task7.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    unsigned int n = 0, fibNum = 0;
+
+    cout << "N: ";
+    cin >> n;
+
+    if (n == 1) {
+        fibNum = 0;
+    } else if (n == 2 || n == 3) {
+        fibNum = 1;
+    } else {
+        unsigned int a = 0, b = 1, c = 1;
+        
+        for (unsigned int i = 4; i <= n; ++i) {
+            a = b;
+            b = c;
+            c = a + b;
+        }
+
+        fibNum = c;
+    }
+
+    cout << "The " << n << "th Fibonacci number is: " << fibNum << endl;
+
+    return 0;
+}
