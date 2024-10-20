d1f6a26b72e6b4723f1585d022b2d520749177a0
Кристиян Мирославов Богданов <71849513+KristiyanBogdanov@users.noreply.github.com>
2024-10-20 19:01:52 +0300

    Add Task5 and update Task4

--- Week 02/Pract. 02/Task4.cpp
+++ Week 02/Pract. 02/Task4.cpp
@@ -2,10 +2,12 @@
 
 using namespace std;
 
+const unsigned int MAX_COUNT_OF_POSITIVES = 5;
+
 int main() {
     unsigned int countOfPositives = 0, sumOfPositives = 0;
 
-    while (countOfPositives != 5) {
+    while (countOfPositives < MAX_COUNT_OF_POSITIVES) {
         int num = 0;
         cin >> num;
 
