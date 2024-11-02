1f71d3a3d6be586957f2d52209704c8cbf519e1c
Zakhar Sobarshov <zakhar.zak@gmail.com>
2024-11-02 11:11:50 +0200

    Separated code to functions

--- Week 04/Pract. 04/Student solutions/Task08.cpp
+++ Week 04/Pract. 04/Student solutions/Task08.cpp
@@ -1,42 +1,45 @@
 #include <iostream>
 
-bool isSym(const int SIZE)
-{ 
-	bool symmetric = 1;
-	const unsigned int MAX_SIZE = 15;
-	char chars[MAX_SIZE] = {};
-
-	for (int increment = 0; increment < SIZE; ++increment)
+char* fillArray(char array[], const int SIZE)
+{
+	for (int i = 0; i < SIZE; ++i)
 	{
-		std::cin >> chars[increment];
+		std::cin >> array[i];
 	}
 
+	return array;
+}
+
+bool isSym(char array[], const int SIZE)
+{
 	for (int startIncrement = 0, endIncrement = SIZE-1; startIncrement < SIZE; ++startIncrement, --endIncrement)
 	{
-		if (chars[startIncrement] != chars[endIncrement])
+		if (array[startIncrement] != array[endIncrement])
 			{
-				symmetric = 0;
+				return 0;
 			}
 	}
 
-	if (symmetric)
-	{
-		std::cout << "Yes, the array is symmetric";
-		return symmetric;
-	}
-	else
-	{
-		std::cout << "No, the array is not symmetric";
-		return symmetric;
-	}
+	return 1;
 }
 
 void main()
 {
-	int numbersQuantity = 0;
+	int charsQuantity = 0;
+	const unsigned int MAX_SIZE = 15;
+	char charsEmpty[MAX_SIZE] = {};
 
-	std::cin >> numbersQuantity;
-	bool checkSym = isSym(numbersQuantity);
+	std::cin >> charsQuantity;
+	char* charsData = fillArray(charsEmpty, charsQuantity);
+
+	bool checkSym = isSym(charsData, charsQuantity);
 	
-	return;
+	if (checkSym)
+	{
+		std::cout << "Yes, the array is symmetric";
+	}
+	else
+	{
+		std::cout << "No, the array is not symmetric";
+	}
 }
