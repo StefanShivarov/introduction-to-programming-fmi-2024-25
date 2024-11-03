e3d469b62c9f828faa97b5ab8ed4baa87659a268
Кристиян Мирославов Богданов <71849513+KristiyanBogdanov@users.noreply.github.com>
2024-11-03 14:45:18 +0200

    Add Task02

---
+++ Week 04/Pract. 04/Student solutions/Task02.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+void deposit(double&, double);
+void withdraw(double&, double);
+
+int main() {
+    double balance = 0.0;
+    deposit(balance, 49.99);
+    cout << balance << endl;
+    withdraw(balance, 55.50);
+    cout << balance << endl;
+    withdraw(balance, 40);
+    cout << balance << endl;
+}
+
+void deposit(double& balance, double amount) {
+    balance += amount;
+}
+
+void withdraw(double& balance, double amount) {
+    if (balance - amount < 0) {
+        cout << "Not enough balance!" << endl;
+    } else {
+        balance -= amount;
+    }
+}
