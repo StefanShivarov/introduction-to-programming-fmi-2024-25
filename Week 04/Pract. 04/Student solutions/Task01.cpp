b41feff7c432a5e1d5f0132b1dd1d19a98266432
Кристиян Мирославов Богданов <71849513+KristiyanBogdanov@users.noreply.github.com>
2024-11-03 14:41:37 +0200

    Add Task01

---
+++ Week 04/Pract. 04/Student solutions/Task01.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+void swap(int&, int&);
+
+int main() {
+    int a = 0, b = 0;
+
+    cout << "Enter a: ";
+    cin >> a;
+
+    cout << "Enter b: ";
+    cin >> b;
+
+    swap(a, b);
+
+    cout << "a: " << a << endl;
+    cout << "b: " << b << endl;
+}
+
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
