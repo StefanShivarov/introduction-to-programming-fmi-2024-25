da798dcdfdcf6554cc3d936b14c8dc0dfed9bb12
Кристиян Мирославов Богданов <71849513+KristiyanBogdanov@users.noreply.github.com>
2024-11-03 15:04:34 +0200

    Update Task02

--- Week 04/Pract. 04/Student solutions/Task02.cpp
+++ Week 04/Pract. 04/Student solutions/Task02.cpp
@@ -7,12 +7,15 @@ void withdraw(double&, double);
 
 int main() {
     double balance = 0.0;
+    
     deposit(balance, 49.99);
     cout << balance << endl;
     withdraw(balance, 55.50);
     cout << balance << endl;
     withdraw(balance, 40);
     cout << balance << endl;
+
+    return 0;
 }
 
 void deposit(double& balance, double amount) {
