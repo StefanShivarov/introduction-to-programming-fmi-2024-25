6af5c043ff1e7f444999812eeb8aa08a90fb45d9
Кристиян Мирославов Богданов <71849513+KristiyanBogdanov@users.noreply.github.com>
2024-11-03 14:56:25 +0200

    Add Task04

---
+++ Week 04/Pract. 04/Student solutions/Task04.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+const unsigned int MAX_SIZE = 50;
+
+double findAverage(const int arr[], const unsigned int n);
+
+int main() {
+    int arr[MAX_SIZE];
+    unsigned int n = 0;
+
+    cout << "N: ";
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    cout << "Average: " << findAverage(arr, n) << endl;
+}
+
+double findAverage(const int arr[], const unsigned int n) {
+    double sum = 0;
+
+    for (int i = 0; i < n; ++i) {
+        sum += arr[i];
+    }
+
+    return sum / n;
+}
