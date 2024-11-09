ba48be770db2cf4f20047658ec15886af963e4dc
Zakhar Sobarshov <zakhar.zak@gmail.com>
2024-11-09 16:42:43 +0200

    Task02

---
+++ Week 05/Pract. 05/Student solutions/Task02.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+void absolute(int& value)
+{
+	if (value < 0) {
+		value *= -1;
+	}
+}
+
+void fillArray(int* array, const int SIZE)
+{
+	for (int i = 0; i < SIZE; ++i)
+	{
+		std::cin >> array[i];
+	}
+}
+
+int* diffNeighbourElements(int* arrayData, int* resultArray, const int SIZE)
+{
+	for (int i = 0; i < SIZE; ++i)
+	{
+		resultArray[i] = arrayData[i] - arrayData[i+1];
+	}
+
+	return resultArray;
+}
+
+void printArray(int* array, const int SIZE)
+{
+	for (int i = 0; i < SIZE; ++i)
+	{
+		std::cout << array[i] << " ";
+	}
+}
+
+void main()
+{
+	int numbersQuantity = 0;
+	int sumsQuantity = 0;
+	const unsigned int MAX_SIZE = 15;
+	int numbersData[MAX_SIZE];
+	int sumsEmpty[MAX_SIZE];
+
+	std::cin >> numbersQuantity;
+	sumsQuantity = numbersQuantity - 1;
+
+	fillArray(numbersData, numbersQuantity);
+	int* sumArray = diffNeighbourElements(numbersData, sumsEmpty, sumsQuantity);
+
+	for (int i = 0; i < sumsQuantity; ++i)
+	{
+		absolute(sumArray[i]);
+	}
+
+	printArray(sumArray, sumsQuantity);
+}
