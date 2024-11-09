6d218f2e24d880e5e7efb30f55e3f8e3e1e8ad50
Zakhar Sobarshov <zakhar.zak@gmail.com>
2024-11-09 16:53:20 +0200

    Task05

---
+++ Week 05/Pract. 05/Student solutions/Task05.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+double absolute(double value)
+{
+	if (value < 0) {
+		return -value;
+	}
+
+	return value;
+}
+
+double returnAverage(int array[], const int SIZE)
+{ 
+	double result = 0;
+
+	for (int i = 0; i < SIZE; ++i)
+	{
+		result += array[i];
+	}
+
+	return result /= SIZE;
+}
+
+void fillArray(int* array, const int SIZE)
+{
+	for (int i = 0; i < SIZE; ++i)
+	{
+		std::cin >> array[i];
+	}
+}
+
+
+int findClosest(int* array, const int SIZE, const double AVERAGE)
+{
+	double diff = 0;
+	int closest = 0;
+
+	for (int i = 0; i < SIZE; ++i)
+	{
+		double diffAbsolute = absolute(array[i] - AVERAGE);
+		
+		if ( !(diff) || diff > diffAbsolute)
+		{
+			diff = diffAbsolute;
+			closest = array[i];
+		}
+	}
+
+	return closest;
+}
+
+void main()
+{
+	int numbersQuantity = 0;
+	const unsigned int MAX_SIZE = 15;
+	int numbers[MAX_SIZE];
+
+	std::cin >> numbersQuantity;
+	fillArray(numbers, numbersQuantity);
+
+	double average = returnAverage(numbers, numbersQuantity);
+	int closest = findClosest(numbers, numbersQuantity, average);
+	std::cout << "Average: " << average << "\n" << closest;
+
+}
