9b77923969988512cccdda44fe55660d49e0eebb
Zakhar Sobarshov <zakhar.zak@gmail.com>
2024-11-11 08:18:03 +0200

    Changed indentation to spaces

--- Week 05/Pract. 05/Student solutions/Task01.cpp
+++ Week 05/Pract. 05/Student solutions/Task01.cpp
@@ -2,51 +2,51 @@
 
 void fillArray(int* array, const int SIZE)
 {
-	for (int i = 0; i < SIZE; ++i)
-	{
-			std::cin >> array[i];
-	}
+ for (int i = 0; i < SIZE; ++i)
+ {
+   std::cin >> array[i];
+ }
 }
 
 int findLongestRow(int* array, const int MAX_SIZE)
 {
-	int longestRow = 0;
-	int prevLongestRow = 0;
+ int longestRow = 0;
+ int prevLongestRow = 0;
 
-	int checkValue = 0;
-	checkValue = array[0];
+ int checkValue = 0;
+ checkValue = array[0];
 
-	for (int i = 0; i < MAX_SIZE; ++i)
-	{
-		if (checkValue != array[i])
-		{
-			checkValue = array[i];
-			if (prevLongestRow < longestRow)
-			{
-				prevLongestRow = longestRow;
-			}
-			longestRow = 0;
-		}
-		++longestRow;
-	}
+ for (int i = 0; i < MAX_SIZE; ++i)
+ {
+  if (checkValue != array[i])
+  {
+   checkValue = array[i];
+   if (prevLongestRow < longestRow)
+   {
+    prevLongestRow = longestRow;
+   }
+   longestRow = 0;
+  }
+  ++longestRow;
+ }
 
-	if (longestRow < prevLongestRow)
-	{
-		return prevLongestRow;
-	}
-	return longestRow;
+ if (longestRow < prevLongestRow)
+ {
+  return prevLongestRow;
+ }
+ return longestRow;
 }
 
 void main()
 {
-	const unsigned int MAX_SIZE = 15;
-	int numbersData[MAX_SIZE];
-	int numbersQuantity = 0;
+ const unsigned int MAX_SIZE = 15;
+ int numbersData[MAX_SIZE];
+ int numbersQuantity = 0;
 
-	std::cin >> numbersQuantity;
+ std::cin >> numbersQuantity;
 
-	fillArray(numbersData, numbersQuantity);
-	int longestRow = findLongestRow(numbersData, numbersQuantity);
+ fillArray(numbersData, numbersQuantity);
+ int longestRow = findLongestRow(numbersData, numbersQuantity);
 
-	std::cout << longestRow << std::endl;
+ std::cout << longestRow << std::endl;
 }
