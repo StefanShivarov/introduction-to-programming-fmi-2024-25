492691e712351c678022bfa8bb6438cbbf74ea07
Zakhar Sobarshov <zakhar.zak@gmail.com>
2024-11-11 08:21:24 +0200

    Changed indentation to spaces

--- Week 05/Pract. 05/Student solutions/Task05.cpp
+++ Week 05/Pract. 05/Student solutions/Task05.cpp
@@ -2,64 +2,64 @@
 
 double absolute(double value)
 {
-	if (value < 0) {
-		return -value;
-	}
+ if (value < 0) {
+  return -value;
+ }
 
-	return value;
+ return value;
 }
 
 double returnAverage(int array[], const int SIZE)
 { 
-	double result = 0;
+ double result = 0;
 
-	for (int i = 0; i < SIZE; ++i)
-	{
-		result += array[i];
-	}
+ for (int i = 0; i < SIZE; ++i)
+ {
+  result += array[i];
+ }
 
-	return result /= SIZE;
+ return result /= SIZE;
 }
 
 void fillArray(int* array, const int SIZE)
 {
-	for (int i = 0; i < SIZE; ++i)
-	{
-		std::cin >> array[i];
-	}
+ for (int i = 0; i < SIZE; ++i)
+ {
+  std::cin >> array[i];
+ }
 }
 
 
 int findClosest(int* array, const int SIZE, const double AVERAGE)
 {
-	double diff = 0;
-	int closest = 0;
+ double diff = 0;
+ int closest = 0;
 
-	for (int i = 0; i < SIZE; ++i)
-	{
-		double diffAbsolute = absolute(array[i] - AVERAGE);
-		
-		if ( !(diff) || diff > diffAbsolute)
-		{
-			diff = diffAbsolute;
-			closest = array[i];
-		}
-	}
+ for (int i = 0; i < SIZE; ++i)
+ {
+  double diffAbsolute = absolute(array[i] - AVERAGE);
+  
+  if ( !(diff) || diff > diffAbsolute)
+  {
+   diff = diffAbsolute;
+   closest = array[i];
+  }
+ }
 
-	return closest;
+ return closest;
 }
 
 void main()
 {
-	int numbersQuantity = 0;
-	const unsigned int MAX_SIZE = 15;
-	int numbers[MAX_SIZE];
+ int numbersQuantity = 0;
+ const unsigned int MAX_SIZE = 15;
+ int numbers[MAX_SIZE];
 
-	std::cin >> numbersQuantity;
-	fillArray(numbers, numbersQuantity);
+ std::cin >> numbersQuantity;
+ fillArray(numbers, numbersQuantity);
 
-	double average = returnAverage(numbers, numbersQuantity);
-	int closest = findClosest(numbers, numbersQuantity, average);
-	std::cout << "Average: " << average << "\n" << closest;
+ double average = returnAverage(numbers, numbersQuantity);
+ int closest = findClosest(numbers, numbersQuantity, average);
+ std::cout << "Average: " << average << "\n" << closest;
 
 }
