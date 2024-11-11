bf2ab200d4c0ca494c6d49cfbed179417a5396a7
Zakhar Sobarshov <zakhar.zak@gmail.com>
2024-11-11 08:18:53 +0200

    Changed indentation to spaces

--- Week 05/Pract. 05/Student solutions/Task02.cpp
+++ Week 05/Pract. 05/Student solutions/Task02.cpp
@@ -2,55 +2,55 @@
 
 void absolute(int& value)
 {
-	if (value < 0) {
-		value *= -1;
-	}
+ if (value < 0) {
+  value *= -1;
+ }
 }
 
 void fillArray(int* array, const int SIZE)
 {
-	for (int i = 0; i < SIZE; ++i)
-	{
-		std::cin >> array[i];
-	}
+ for (int i = 0; i < SIZE; ++i)
+ {
+  std::cin >> array[i];
+ }
 }
 
 int* diffNeighbourElements(int* arrayData, int* resultArray, const int SIZE)
 {
-	for (int i = 0; i < SIZE; ++i)
-	{
-		resultArray[i] = arrayData[i] - arrayData[i+1];
-	}
+ for (int i = 0; i < SIZE; ++i)
+ {
+  resultArray[i] = arrayData[i] - arrayData[i+1];
+ }
 
-	return resultArray;
+ return resultArray;
 }
 
 void printArray(int* array, const int SIZE)
 {
-	for (int i = 0; i < SIZE; ++i)
-	{
-		std::cout << array[i] << " ";
-	}
+ for (int i = 0; i < SIZE; ++i)
+ {
+  std::cout << array[i] << " ";
+ }
 }
 
 void main()
 {
-	int numbersQuantity = 0;
-	int sumsQuantity = 0;
-	const unsigned int MAX_SIZE = 15;
-	int numbersData[MAX_SIZE];
-	int sumsEmpty[MAX_SIZE];
+ int numbersQuantity = 0;
+ int sumsQuantity = 0;
+ const unsigned int MAX_SIZE = 15;
+ int numbersData[MAX_SIZE];
+ int sumsEmpty[MAX_SIZE];
 
-	std::cin >> numbersQuantity;
-	sumsQuantity = numbersQuantity - 1;
+ std::cin >> numbersQuantity;
+ sumsQuantity = numbersQuantity - 1;
 
-	fillArray(numbersData, numbersQuantity);
-	int* sumArray = diffNeighbourElements(numbersData, sumsEmpty, sumsQuantity);
+ fillArray(numbersData, numbersQuantity);
+ int* sumArray = diffNeighbourElements(numbersData, sumsEmpty, sumsQuantity);
 
-	for (int i = 0; i < sumsQuantity; ++i)
-	{
-		absolute(sumArray[i]);
-	}
+ for (int i = 0; i < sumsQuantity; ++i)
+ {
+  absolute(sumArray[i]);
+ }
 
-	printArray(sumArray, sumsQuantity);
+ printArray(sumArray, sumsQuantity);
 }
