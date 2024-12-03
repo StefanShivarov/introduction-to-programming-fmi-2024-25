6e243777615c5ac8dc3b85a6268a9896b94e02e9
Кристиян Мирославов Богданов <71849513+KristiyanBogdanov@users.noreply.github.com>
2024-12-04 00:15:57 +0200

    Add Task01

---
+++ Week 09/Pract. 09/Task01.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+using namespace std;
+
+const size_t MAX_SIZE = 100;
+
+char* myStrcpy(char* dest, const char* src);
+char* myStrcat(char* dest, const char* src);
+size_t myStrlen(const char* str);
+
+int main() {
+    char str1[] = "Hello, World!";
+    char str2[MAX_SIZE];
+
+    myStrcpy(str2, str1);
+
+    cout << "[ORIGINAL] str1: " << str1 << endl;
+    cout << "[AFTER STRCPY] str2: " << str2 << endl;
+
+    myStrcat(str2, " Goodbye, World!");
+
+    cout << "[AFTER STRCAT] str2: " << str2 << endl;
+
+    cout << "Length of str1: " << myStrlen(str1) << endl;
+
+    return 0;
+}
+
+char* myStrcpy(char* dest, const char* src) {
+    char* originalDest = dest;
+    
+    while ((*dest = *src)) {
+        ++dest;
+        ++src;
+    }
+    
+    return originalDest;
+}
+
+char* myStrcat(char* dest, const char* src) {
+    char* originalDest = dest;
+    
+    while (*dest) {
+        ++dest;
+    }
+    
+    while ((*dest = *src)) {
+        ++dest;
+        ++src;
+    }
+    
+    return originalDest;
+}
+
+size_t myStrlen(const char* str) {
+    size_t length = 0;
+    
+    while (*str) {
+        ++str;
+        ++length;
+    }
+    
+    return length;
+}
