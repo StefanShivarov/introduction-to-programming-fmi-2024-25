5cdc21f8b2f7f2b82729f0f816ac93655178574f
Zakhar Sobarshov <zakhar.zak@gmail.com>
2024-12-09 10:35:53 +0200

    Task07

--- Week 09/Pract. 09/Task07.cpp
+++ Week 09/Pract. 09/Task07.cpp
@@ -1,23 +1,93 @@
 #include <iostream>
 
-bool returnBit (size_t num, size_t pos);
+const size_t MAX_SIZE = 30;
 
-int main ()
+char convertNumberToSymbol(size_t number);
+void convertDecToKth(char *array, size_t number, size_t system);
+void strcopyend(char *source, char *dest, int pos);
+
+int main()
 {
-	size_t num = 0;
-	size_t pos = 0;
+	char result[MAX_SIZE];
+	size_t number = 0;
 
-	std::cout << "Number: ";
-	std::cin >> num;
-	std::cout << "Position: ";
-	std::cin >> pos;
+	std::cin >> number;
+	convertDecToKth(result, number, 16);
 
-	bool bit = returnBit(num, pos);
+	std::cout << result << std::endl;
 
-	bit ? std::cout << "1" : std::cout << "0";
+	return 0;
 }
 
-bool returnBit (size_t num, size_t pos)
+char convertNumberToSymbol(size_t number)
 {
-	return num & (1 << pos) ? 1 : 0;
+	char symbol = ' ';
+
+	if (number >= 0 && number <= 9)
+	{
+		return symbol = number + '0';
+	}
+	if (number > 9)
+	{
+		return symbol = number - 10 + 'A';
+	}
+
+	return symbol;
+}
+
+void convertDecToKth(char *array, size_t number, size_t system)
+{
+	char symbol = ' ';
+	int i = 0;
+	char tempResult[MAX_SIZE];
+
+	while (number)
+	{
+		if (number >= system)
+		{
+			if (number % system)
+			{
+				symbol = convertNumberToSymbol(number % system);
+				*(tempResult + i) = symbol;
+				number -= number % system;
+				number /= system;
+				++i;
+			}
+			else
+			{
+				symbol = convertNumberToSymbol(0);
+				*(tempResult + i) = symbol;
+				number /= system;
+				++i;
+			}
+		}
+		else
+		{
+			symbol = convertNumberToSymbol(number);
+			*(tempResult + i) = symbol;
+			number = 0;
+			++i;
+		}
+	}
+	
+	strcopyend(tempResult, array, i);
+
+	*(tempResult + i) = '\0';
+	i++;
+}
+
+void strcopyend(char *source, char *dest, int pos)
+{
+	size_t i = 0;
+	pos--;
+
+	while(pos >= 0)
+	{
+		*(dest + i) = *(source + pos);
+		pos--;
+		i++;
+	}
+
+	*(dest + i) = '\0';
+	i++;
 }
